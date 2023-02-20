b4458ffabc7356dfa0e026e24fbe592f45673176
youj <devyouj@gmail.com>
2023-02-20 16:05:38 +0900

    section4: 스프링 빈과 의존관계 - 컴포넌트 스캔과 자동 의존관계 설정

---
+++ src/main/java/com/youj/hellospring/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.youj.hellospring.controller;
+
+import com.youj.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/com/youj/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/youj/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.youj.hellospring.repository;
 
 import com.youj.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
